726a6548130620383f9eb8f3923edda759546ea6
UyghurTekin <tekinnn008@gmail.com>
2023-02-24 00:53:12 +0300

    Sayi Basamak Toplama
    Girilen sayının basamaklarının toplamını veriyor...

---
+++ basamakToplam.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class basamakToplam {
+    public static void main(String[] args) {
+    int sayi , toplam  = 0 ;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayi Girin:");
+        sayi = inp.nextInt();
+
+        while (sayi != 0){
+            toplam += (sayi % 10) ;
+            System.out.println("Toplamm: " + toplam);
+            sayi /= 10 ;
+            System.out.println("sayi: " + sayi);
+        }
+
+        System.out.println("Toplam: " + toplam);
+    }
+}
